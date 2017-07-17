8a8124cf525c6f252953a30b76090484a0acd075
andriilguvi <andriilguvi68@gmail.com>
2017-07-17 10:53:20 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class count
+{
+public static void main(String args[])
+{
+int count=0,a;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a<0)
+{
+a=a*-1;
+}
+else if
+{
+a==1;
+}
+while(a>0)
+{
+a=a/10;
+count++;
+}
+System.out.print(count);
+}
+}
